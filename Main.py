38442fde15e444f3dbd4924b5a8d36489acf63a4
Hamza <hamzajamil6210@gmail.com>
2025-11-11 23:14:53 +0000

    Input taken

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+#print("Hello World" , end=" ")
+#print("Hi World")
+
+#print("Hi my name is Hamza and I'm" , 35 , "years old")
+
+#x = 45
+#y = 35 
+#if x < y :
+#    print("y is greater than x")
+#else:
+#    print("x is greater than y")
+
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")   
+sum = float(num1) + float(num2)
+
+if num1 | num2 == input(float):
+    print(f"The sum is: {float(sum)}")
+
+else:
+    print(f"The sum is: {sum}")
