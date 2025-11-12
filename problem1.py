03968fa0108ec4ff1987f5804b46b8841490666e
Hamza <hamzajamil6210@gmail.com>
2025-11-12 23:16:02 +0000

    New problem

---
+++ problem1.py
@@ -0,0 +1,13 @@
+#Ask the user to input a maximum age. 
+#Then fetch user data from the API and print a summary of users who are younger
+#than that age. Print their full name, email, age, and a message like 
+#"This user is young" or "This user is middle-aged" depending on age
+
+#Name: Jack Smith
+#Age: 25
+#Email: jack@example.com
+#This user is young.
+
+#https://dummyjson.com/users
+
+print("Hi")
